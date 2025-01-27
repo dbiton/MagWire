1158054e4104c41339b70bf13cda63e51fca338b
Your Name <your.email@example.com>
2025-01-27 16:41:58 +0200

    Add features to robot simulator

--- robot_simulator.py
+++ robot_simulator.py
@@ -5,7 +5,18 @@ from keras.models import load_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from robot_interface import RobotInterface
-from robot_controller import BaseRobot
+
+def transform_to_euler_angles(matrix):
+    assert matrix.shape == (4, 4), "Input must be a 4x4 matrix"
+    R = matrix[:3, :3]
+    yaw = np.arctan2(R[1, 0], R[0, 0])
+    pitch = np.arcsin(-R[2, 0])
+    roll = np.arctan2(R[2, 1], R[2, 2])
+    return np.array([yaw, pitch, roll])
+
+def transform_to_position(matrix):
+    assert matrix.shape == (4, 4), "Input must be a 4x4 matrix"
+    return matrix[:, -1][:3]
 
 class RobotSimulator(RobotInterface):
     def __init__(self, save_model_path = "robot_simulator_model.h5", load_model_path = None):
@@ -44,26 +55,36 @@ class RobotSimulator(RobotInterface):
         return self.magwire_pos
 
     def process_data(self, data: dict):
-        X = []
-        y = []
+        data['actuator_transform'] = data['robot'].apply(self.estimate_actuator_transform)
+        data['actuator_eular'] = data['actuator_transform'].apply(transform_to_euler_angles)
+        data['actuator_pos'] = data['actuator_transform'].apply(transform_to_position)
+        # Calculate distance moved
+        data['actuator_diff'] = data['actuator_pos'].apply(np.array).diff().bfill()
+        data['actuator_delta'] = data['actuator_pos'].apply(np.array).diff().apply(lambda x: np.linalg.norm(x) if x is not None else 0).fillna(0)
+        data['time_diff'] = data['time'].diff().bfill()
+        data['actuator_velocity'] = (data['actuator_delta'] / data['time_diff']).fillna(0)
+        data['actuator_velocity_diff'] = data['actuator_velocity'].diff().fillna(0)
+        data['actuator_acceleration'] = (data['actuator_velocity_diff'] / data['time_diff']).fillna(0)
+
         seconds_past = 1
         seconds_future = 1
         mean_dt = np.mean(np.diff(data['time']))
-        data['actuator'] = data['robot'].apply(self.estimate_actuator_transform)
+        
         frames_count_past = int(seconds_past / mean_dt)
         frames_count_future = int(seconds_future / mean_dt)
+
+        y = []
+        X = []
+
         for i_frame in range(frames_count_past, len(data) - frames_count_future):
             frames = data[i_frame-frames_count_past:i_frame+frames_count_future]
             frames_past = data[i_frame-frames_count_past:i_frame]
-            frames_future = data[i_frame:i_frame+frames_count_future]
+            frames_future = data[i_frame:i_frame+frames_count_future].copy()
             magwire_pos_future = np.stack(frames_future["wire"].to_numpy()).flatten()
             y.append(magwire_pos_future)
-            robot_pos = np.stack(frames["robot"].to_numpy())
-            actuator_pos = np.stack([np.array(e).flatten() for e in frames["actuator"]])
-            magwire_pos_past = np.stack(frames_past["wire"].to_numpy())
-            magwire_pos_past_padding = np.zeros((len(frames)-len(magwire_pos_past), 2))
-            magwire_pos_past = np.concatenate((magwire_pos_past, magwire_pos_past_padding), axis=0)
-            features = np.concatenate((magwire_pos_past, robot_pos, actuator_pos), axis=1)
+            frames_future['wire'] = [np.array([0, 0]) for _ in range(len(frames_future))]
+            features = np.concatenate((frames_past.to_numpy(), frames_future.to_numpy()))
+            features = [np.concatenate([np.array(v).flatten() for v in f]) for f in features]
             X.append(features)        
         return np.array(X), np.array(y)
 
@@ -78,7 +99,7 @@ class RobotSimulator(RobotInterface):
             hidden_units = 64
             self.build_model(input_shape, output_length, hidden_units)
 
-        self.model.fit(X_train, y_train, epochs=128, batch_size=512, validation_split=0.1)
+        self.model.fit(X_train, y_train, epochs=32, batch_size=32, validation_split=0.1)
 
         if self.save_model_path:
             self.model.save(self.save_model_path)
