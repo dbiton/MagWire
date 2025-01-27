54a2ffa79f9ac89c163485ae0eeb4447c9745aae
Dvir David Biton <dbiton@gmail.com>
2025-01-27 11:24:26 +0200

    Implement process frames

---
+++ process_frames.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+def process_frames(frames_dir, output_file):
+    frames_names = [f for f in os.listdir(frames_dir) if f.endswith(".png")]
+    frames_times = sorted([float(f[:-4]) for f in frames_names])
+    os.system(f'cd {frames_dir} && ffmpeg -framerate 30 -pattern_type glob -i "*.png" -c:v libx264 -pix_fmt yuv420p {output_file}')
+    with open(f"{frames_dir}\\frame_times.json", "w") as f:
+        json.dump(frames_times, f)
+
+if __name__=="__main__":
+    process_frames("C:\\Users\\User\\Desktop\\frames", "output.mp4")
